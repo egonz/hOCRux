b82c0b5853f6a22dd0882b583952f2880da2e360
Eddie Gonzales <eddiemgonzales@gmail.com>
2012-03-21 22:10:58 -0700

    working on the edocs show view

--- app/controllers/edocs_controller.rb
+++ app/controllers/edocs_controller.rb
@@ -2,7 +2,9 @@ class EdocsController < ApplicationController
 	before_filter :authenticate_user!
 
 	def show
-
+		edoc_id = params[:id]
+		logger.info "Edoc ID #{edoc_id}"
+		@edoc = Edoc.find(edoc_id)
 	end
 
 end
